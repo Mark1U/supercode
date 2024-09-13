ef9fda2a639cf88e1058757860a6c9eb1cff9022
Mark1U <mark@zurcloud.com>
2024-09-13 12:09:47 +0200

    D14

---
+++ D14_JS/assets/js/1_11.js
@@ -0,0 +1,13 @@
+let x = "Hello";
+let y = "World";
+let z = x + y;
+document.write("z: " + z);
+let n = x + " " + y;
+document.write("n: " + n);
+
+let meinString = "Ich bin Erster";
+meinString += " Ich komme auf Platz zwei";
+document.write("meinString: " + meinString);
+
+
+document.write(`Methode: ${x} ${y}`);
---
+++ D14_JS/assets/js/1_12.js
@@ -0,0 +1,28 @@
+let score = 5 + 5 * 10;  
+console.log('Ergebnis: ' + score);
+
+let score2 = (5 + 5) * 10;  
+console.log('Ergebnis(2): ' + score2);
+
+let score3 = 0;  
+score3 = score3 + 10;  
+console.log('Ergebnis(3): ' + score3);
+
+
+score3 += 10;  
+console.log('Ergebnis(3b): ' + score3);
+
+let zahl = 1;  
+
+zahl = zahl + 1;
+console.log('increment: ' + zahl);  
+
+zahl += 1;
+console.log('increment: ' + zahl);
+
+zahl++;  
+console.log('increment: ' + zahl);
+
+zahl--;  
+console.log('decrement: ' + zahl);
+
